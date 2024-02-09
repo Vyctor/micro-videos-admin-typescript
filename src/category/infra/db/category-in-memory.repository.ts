a7825158b20c099d6caf309d7bbb3524b6a94184
Vyctor <dev.vyctor@gmail.com>
2024-02-09 00:19:50 -0300

    Criando repositório buscável em memória para Categoria

--- src/category/infra/db/category-in-memory.repository.ts
+++ src/category/infra/db/category-in-memory.repository.ts
@@ -1,12 +1,35 @@
-import { InMemoryRepository } from "../../../shared/infra/db/in-memory/in-memory.repository";
+import { SortDirection } from "../../../shared/domain/repository/search-params";
+import { InMemorySearchableRepository } from "../../../shared/infra/db/in-memory/in-memory.repository";
 import { Category } from "../../domain/category.entity";
 import { Uuid } from "../../domain/value-objects/uuid.vo";
 
-export class CategoryInMemoryRepository extends InMemoryRepository<
+export class CategoryInMemoryRepository extends InMemorySearchableRepository<
   Category,
   Uuid
 > {
+  sortableFields: string[] = ["name", "created_at"];
+
+  protected async applyFilter(
+    items: Category[],
+    filter: string
+  ): Promise<Category[]> {
+    if (!filter) return Promise.resolve(items);
+    return items.filter((i) => {
+      return i.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
+
   getEntity(): new (...args: any[]) => Category {
     return Category;
+  }
+
+  protected applySort(
+    items: Category[],
+    sort: string | null,
+    sort_dir: SortDirection | null
+  ): Array<Category> {
+    return sort
+      ? super.applySort(items, sort, sort_dir)
+      : super.applySort(items, "created_at", "desc");
   }
 }
