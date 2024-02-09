3184b285f93871f411e894a8f0b88e7ed5634cbb
Vyctor <dev.vyctor@gmail.com>
2024-02-08 23:18:24 -0300

    Testando abstração de entrada e saida de busca em repositórios

--- src/shared/domain/repository/search-params.ts
+++ src/shared/domain/repository/search-params.ts
@@ -6,27 +6,27 @@ export type SearchParamsConstructorProps<Filter = string> = {
   page?: number;
   per_page?: number;
   sort?: string | null;
-  sort_direction?: SortDirection | null;
+  sort_dir?: SortDirection | null;
   filter?: Filter | null;
 };
 
 export class SearchParams<Filter = string> extends ValueObject {
   protected _page: number;
-  protected _per_page: number;
+  protected _per_page: number = 15;
   protected _sort: string | null;
-  protected _sort_direction: SortDirection | null;
+  protected _sort_dir: SortDirection | null;
   protected _filter: Filter | null;
 
   constructor(props: SearchParamsConstructorProps<Filter> = {}) {
     super();
-    this._page = props.page || 1;
-    this._per_page = props.per_page || 10;
-    this._sort = props.sort || null;
-    this._sort_direction = props.sort_direction || null;
-    this._filter = props.filter || null;
+    this.page = props.page!;
+    this.per_page = props.per_page!;
+    this.sort = props.sort!;
+    this.sort_dir = props.sort_dir!;
+    this.filter = props.filter!;
   }
 
-  get page(): number {
+  get page() {
     return this._page;
   }
 
@@ -40,7 +40,7 @@ export class SearchParams<Filter = string> extends ValueObject {
     this._page = _page;
   }
 
-  get per_page(): number {
+  get per_page() {
     return this._per_page;
   }
 
@@ -54,6 +54,7 @@ export class SearchParams<Filter = string> extends ValueObject {
     ) {
       _per_page = this._per_page;
     }
+
     this._per_page = _per_page;
   }
 
@@ -66,18 +67,17 @@ export class SearchParams<Filter = string> extends ValueObject {
       value === null || value === undefined || value === "" ? null : `${value}`;
   }
 
-  get sort_direction(): SortDirection | null {
-    return this._sort_direction;
+  get sort_dir(): SortDirection | null {
+    return this._sort_dir;
   }
 
-  private set sort_direction(value: SortDirection | null) {
+  private set sort_dir(value: SortDirection | null) {
     if (!this.sort) {
-      this._sort_direction = null;
+      this._sort_dir = null;
       return;
     }
-    const direction = `${value}`.toLowerCase();
-    this._sort_direction =
-      direction !== "asc" && direction !== "desc" ? "asc" : direction;
+    const dir = `${value}`.toLowerCase();
+    this._sort_dir = dir !== "asc" && dir !== "desc" ? "asc" : dir;
   }
 
   get filter(): Filter | null {
