4e7d29ad8df1bf88d863d00d0f9f80835f897c1a
Vyctor <dev.vyctor@gmail.com>
2024-03-08 00:23:08 -0300

    Adicionando threshold de coverage

--- jest.config.ts
+++ jest.config.ts
@@ -9,6 +9,14 @@ const config: Config = {
   },
   rootDir: "./src",
   coverageDirectory: "../coverage",
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
 };
 
 export default config;
