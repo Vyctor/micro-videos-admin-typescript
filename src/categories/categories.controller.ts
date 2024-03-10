5fe656a48e445b66627d5d53edb697ff44fb11b7
Vyctor <dev.vyctor@gmail.com>
2024-03-10 16:30:44 -0300

    Primeiro teste do controller de Categoria

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -9,10 +9,13 @@ import {
 } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
 
 @Controller('categories')
 export class CategoriesController {
-  constructor() {}
+  constructor(
+    private readonly categorySequelizeRepository: CategorySequelizeRepository,
+  ) {}
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {}
