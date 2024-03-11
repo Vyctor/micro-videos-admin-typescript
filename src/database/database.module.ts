030eb7c5db35279ba464d75af7b22baa4460a67e
Vyctor <dev.vyctor@gmail.com>
2024-03-10 22:42:12 -0300

    Usando ConfigService no módulo Database

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,16 +1,43 @@
 import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { CONFIG_SCHEMA_TYPE } from 'src/config/config.module';
 
 const models = [CategoryModel];
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'sqlite' as any,
-      host: ':memory',
-      logging: false,
-      models,
+    SequelizeModule.forRootAsync({
+      useFactory: (configService: ConfigService<CONFIG_SCHEMA_TYPE>) => {
+        const DB_VENDOR = configService.get('DB_VENDOR');
+
+        if (DB_VENDOR === 'mysql') {
+          return {
+            dialect: DB_VENDOR,
+            host: configService.get('DB_HOST'),
+            port: configService.get('DB_PORT'),
+            username: configService.get('DB_USERNAME'),
+            password: configService.get('DB_PASSWORD'),
+            database: configService.get('DB_DATABASE'),
+            storage: configService.get('DB_DATABASE'),
+            logging: configService.get('DB_LOGGING'),
+            autoLoadModels: configService.get('DB_AUTO_LOAD_MODELS'),
+            models,
+          };
+        }
+        if (DB_VENDOR === 'sqlite') {
+          return {
+            dialect: DB_VENDOR,
+            storage: configService.get('DB_DATABASE'),
+            logging: configService.get('DB_LOGGING'),
+            autoLoadModels: configService.get('DB_AUTO_LOAD_MODELS'),
+            models,
+          };
+        }
+        throw new Error(`DB_VENDOR ${DB_VENDOR} not supported`);
+      },
+      inject: [ConfigService],
     }),
   ],
 })
