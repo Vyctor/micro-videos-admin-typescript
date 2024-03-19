105390a7872b16f79970d32b2caf58219673102d
Vyctor <dev.vyctor@gmail.com>
2024-03-18 22:56:14 -0300

    Usando providers no controller de Categoria

--- src/nest-modules/categories-module/categories.controller.ts
+++ src/nest-modules/categories-module/categories.controller.ts
@@ -6,16 +6,32 @@ import {
   Patch,
   Param,
   Delete,
+  Inject,
 } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
-import { CategorySequelizeRepository } from '@core/category/infra/db/sequelize/category-sequelize.repository';
+import { CreateCategoryUsecase } from '@core/category/application/usecases/create-category/create-category.usecase';
+import { UpdateCategoryUsecase } from '@core/category/application/usecases/update-category/update-category.usecase';
+import { DeleteCategoryUsecase } from '@core/category/application/usecases/delete-category/delete-category.usecase';
+import { GetCategoryUsecase } from '@core/category/application/usecases/get-category/get-category.usecase';
+import { ListCategoriesUsecase } from '@core/category/application/usecases/list-categories/list-categories.usecase';
 
 @Controller('categories')
 export class CategoriesController {
-  constructor(
-    private readonly categorySequelizeRepository: CategorySequelizeRepository,
-  ) {}
+  @Inject(CreateCategoryUsecase)
+  private createCategoryUsecase: CreateCategoryUsecase;
+
+  @Inject(UpdateCategoryUsecase)
+  private updateCategoryUsecase: UpdateCategoryUsecase;
+
+  @Inject(DeleteCategoryUsecase)
+  private deleteCategoryUsecase: DeleteCategoryUsecase;
+
+  @Inject(GetCategoryUsecase)
+  private getCategoryUsecase: GetCategoryUsecase;
+
+  @Inject(ListCategoriesUsecase)
+  private listCategoriesUsecase: ListCategoriesUsecase;
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {}
